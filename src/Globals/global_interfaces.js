df97954ff87aa62cf285543762dde27f3042025b
Kyle Morehead <kyle@limelighthealth.com>
2021-09-25 17:58:54 -0700

    fix: cleanup

--- src/Globals/global_interfaces.js
+++
@@ -1,7 +0,0 @@
-(function (API_Request_Types) {
-    API_Request_Types[API_Request_Types["Post"] = 'POST'] = "Post";
-    API_Request_Types[API_Request_Types["Get"] = 'GET'] = "Get";
-    API_Request_Types[API_Request_Types["Put"] = 'PUT'] = "Put";
-    API_Request_Types[API_Request_Types["Delete"] = 'Delete'] = "Delete";
-})(exports.API_Request_Types || (exports.API_Request_Types = {}));
-var API_Request_Types = exports.API_Request_Types;
